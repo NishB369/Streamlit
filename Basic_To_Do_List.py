ef5d68340661f2181e68df474e50ca94b7125001
NishB369 <147968035+NishB369@users.noreply.github.com>
2023-11-22 20:29:09 +0530

    Create Basic_To_Do_List.py

---
+++ Basic_To_Do_List.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from datetime import date
+
+st.title('To-Do List')
+
+# For current date display
+cdt = date.today()
+st.subheader('{ ' + str(cdt) + ' }')
+
+st.markdown("## *Welcome! Plan your day ahead*.🔥")
+st.image("sunrise.jpg")
+
+st.markdown('## *Motivate yourself 💪*')
+a=st.text_input('Enter a quote:')
+if a!='':
+    st.text('That\'s a great start!')
+else:
+    st.text('Give it a Try!')
+
+st.markdown('## ***Tasks***🎯')
+n = st.text_input('Enter the number of tasks:', '0')
+n = int(n)
+
+l = []
+for i in range(n):
+    tsk = st.text_input('Enter the task:',placeholder='Enter a task!',key=i)
+    l.append(tsk)
+
+st.markdown('## *List*')
+
+chk=''
+for j,i in enumerate(l):
+    if i!='':
+        chk=st.checkbox(i)
+    if chk:
+        st.text(f'Great! 👏\nTask {j+1} : "{i}" : IS DONE')
